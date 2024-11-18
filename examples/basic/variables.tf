cbee44773c0e506c215b13d77fa3f34525a2df6b
Jared Holgate <jaredholgate@microsoft.com>
2024-11-18 10:51:20 +0000

    Tidy examples

--- examples/basic/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  type    = string
-  default = "westus2"
-}
-
-variable "suffix" {
-  type    = string
-  default = "test"
-}
--- examples/startup/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "spoke2_pip" {
-  depends_on = [module.vm_spoke2]
-  value      = module.vm_spoke2.public_ips["network_interface_1-ip_configurations_1"].ip_address
-}
