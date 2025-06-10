eadc0422b8209033bcad52431b8fde267ed8bfba
Jared Holgate <jaredholgate@microsoft.com>
2025-06-10 22:40:19 +0100

    fix bug

--- locals.firewall.tf
+++ locals.firewall.tf
@@ -19,7 +19,7 @@ locals {
         name = try(coalesce(vnet.firewall.default_ip_configuration.name, "default"), "default")
       }
       management_ip_enabled = try(vnet.firewall.management_ip_enabled, true)
-      management_ip_configuration = try(vnet.firewall.management_ip_enabled, true) ? {} : {
+      management_ip_configuration = {
         name = try(coalesce(vnet.firewall.management_ip_configuration.name, "defaultMgmt"), "defaultMgmt")
       }
       zones = vnet.firewall.zones
